ec4fd81d6f7272d2603e517fefc456806a1a85bf
Anita <annaaleinik1807@gmail.com>
2018-07-19 07:42:47 +0300

    refactoring

--- MoviesTest/Controllers/InfoFilmVC.swift
+++ MoviesTest/Controllers/InfoFilmVC.swift
@@ -23,20 +23,37 @@ class InfoFilmVC: UIViewController {
     
     
     override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+   
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setup() 
+    }
+
+    func setup () {
+        
+        self.getTrailer()
         self.languageLable.text = modelFilm?.originalLanguage
         self.releaseDateLabel.text = modelFilm?.releaseDate
-        self.popularityLabel.text = String(describing: modelFilm?.popularity)
+        let rating = modelFilm?.popularity ?? 0.0
+        self.popularityLabel.text = String(describing: rating)
         self.descriptionTextView.text = modelFilm?.overview
-        
+        self.navigationItem.title = modelFilm?.originalTitle
+        self.navigationController?.navigationBar.tintColor = UIColor(displayP3Red: 128/255, green: 92/255, blue: 232/255, alpha: 1)
+
         let posterPath = modelFilm?.posterPath
         guard let poster = posterPath else {return}
         let link = APIConst.phosterURL + poster
         self.posterImg.downloadedFrom(link: link)
-        
+    }
+    
+    func getTrailer() {
         guard let id = modelFilm?.id else {return}
         let filmId = String(id)
-        
-        NetWorkManager.sharedInstance.getTrailer(filmId: filmId) { (film, error) in
+        NetWorkManager.sharedInstance.getTrailer(filmId: filmId) { [weak self] (film, error) in
+            guard let `self` = self else { return }
             guard error == nil else {
                 print(error?.localizedDescription)
                 return
@@ -44,16 +61,6 @@ class InfoFilmVC: UIViewController {
             guard let idTrailer = film?.first?.key else {return}
             self.videoPlayer.loadVideoID(idTrailer)
         }
-        
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.navigationItem.title = modelFilm?.originalTitle
-        self.navigationController?.navigationBar.tintColor = UIColor(displayP3Red: 128/255, green: 92/255, blue: 232/255, alpha: 1)
-       
-    }
-
-    
 
 }
